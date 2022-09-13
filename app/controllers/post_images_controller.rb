66fb7b2b2b9a49e3fb35667decfab8de220e1f8a
EC2 Default User <ec2-user@ip-172-31-2-223.ap-northeast-1.compute.internal>
2022-09-13 20:25:50 +0000

    [add]meshiterro

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,11 @@
+class PostImagesController < ApplicationController
+  def new
+    @post_image = post_image.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "homes#top"
+  
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
+  end
