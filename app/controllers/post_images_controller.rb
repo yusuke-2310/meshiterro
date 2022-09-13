62622a218c8c087cf6588117328d73ae851b3726
EC2 Default User <ec2-user@ip-172-31-2-223.ap-northeast-1.compute.internal>
2022-09-13 20:49:22 +0000

    [update]meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,13 @@
 class PostImagesController < ApplicationController
   def new
-    @post_image = post_image.new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -8,4 +15,15 @@ class PostImagesController < ApplicationController
 
   def show
   end
+  
+  def destroy
+  end
+  
+  
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
 end
